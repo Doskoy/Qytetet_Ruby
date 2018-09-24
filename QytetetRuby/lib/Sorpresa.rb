6de8afbc22275a39a7d4ddad36f080fdd5ded73c
Fernando Roldán Zafra <Doskoy93@gmail.com>
2018-09-24 21:00:38 +0200

    Hasta el punto 4
    Hasta el punto 4

---
+++ QytetetRuby/lib/Sorpresa.rb
@@ -0,0 +1,26 @@
+#encoding: utf-8
+module ModeloQytetet
+  class Sorpresa
+    def initialize(sorpresa, valor, tipo)
+    @sorpresa = sorpresa
+    @tipo = tipo
+    @valor = valor
+    end
+    
+    def sorpresa
+      @sorpresa
+    end
+    
+    def tipo
+      @tipo
+    end
+    
+    def valor
+      @valor
+    end
+    
+    def to_s
+      "Texto: #{@texto} \n Valor: #{@valor} \n Tipo: #{@tipo}"
+    end
+  end
+end
