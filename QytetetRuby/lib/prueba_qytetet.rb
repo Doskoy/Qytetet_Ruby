52bd459fafafd8b13a654633732f49aef701ac40
Fernando Roldán Zafra <Doskoy93@gmail.com>
2018-09-25 15:50:10 +0200

    Añadidos Require relative
    Resolucion de dudas despues de tutoria

--- QytetetRuby/lib/prueba_qytetet.rb
+++ QytetetRuby/lib/prueba_qytetet.rb
@@ -1,2 +1,23 @@
 #encoding: utf-8
+require_relative "Qytetet"
+module ModeloQytetet
+  class PruebaQytetet
+    @@juego = Qytetet.new
 
+    def main
+     puts "hola"       
+    end
+   
+    def ValorMayorQueCero
+         @@juego.mazo = Array.new
+         
+   
+        
+      end
+    
+  end
+  puts "hola1"
+  x = PruebaQytetet.new
+  x.main
+  
+end
