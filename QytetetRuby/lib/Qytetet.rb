432a38872ddfe8b95ed1ceeec15983846da8b7ed
manu <manu@ManuMINT>
2018-09-27 16:35:28 +0200

    Eliminada carta repetida de la cárcel

--- QytetetRuby/lib/Qytetet.rb
+++ QytetetRuby/lib/Qytetet.rb
@@ -6,8 +6,6 @@ module ModeloQytetet
   class Qytetet
     attr_reader :mazo
     
-    #asd
-    
     def inicializarCartasSorpresa
       @mazo = Array.new
       @mazo << Sorpresa.new("El banco se ha equivocado en algunas cuentas y te devuelven dinero. Es que no estudian...", 250, TipoSorpresa::PAGARCOBRAR)
