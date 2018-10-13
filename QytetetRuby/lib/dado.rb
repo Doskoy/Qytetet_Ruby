c20acdb74b57ae3de92224ceafa3e1837e67d6ed
manu <manu@ManuMINT>
2018-10-13 20:19:09 +0200

    Terminada clase "Dado"

---
+++ QytetetRuby/lib/dado.rb
@@ -0,0 +1,15 @@
+require "singleton"
+module ModeloQytetet
+  class Dado
+      include Singleton
+      attr_reader:valor
+      
+      def initialize
+        @valor = 0
+      end
+      
+      def tirar
+        raise NotImplementedError        
+      end
+  end
+end
